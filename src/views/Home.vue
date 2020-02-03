557a88de9e49823d8aac8e65afc7074c5fec0c71
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-03 06:11:43 -0800

    Start of VSRating

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
+    <VSRating :rating="3" />
+    <hr>
     <VSButton />
   </div>
 </template>
 
 <script>
 import VSButton from '@/components/VSButton';
+import VSRating from '@/components/VSRating';
 
 export default {
   name: 'home',
   components: {
-    VSButton
+    VSButton,
+    VSRating
   }
 }
 </script>
