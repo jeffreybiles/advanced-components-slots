3412adb1c29b493037461a2aa5a3dacc1e64ea34
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-03 05:45:15 -0800

    Starter VSButton

---
+++ src/components/VSButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn">
+    Button
+  </button>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+  button {
+    position: relative;
+    padding: 15px;
+    border-radius: 5px;
+    font-size: 20px;
+    background-color: rgb(158, 195, 208);
+    border-color: black;
+    box-shadow: 2px 2px rgb(39, 39, 196);
+    cursor: pointer;
+
+    &:hover {
+      box-shadow: 5px 5px rgb(39, 39, 196);
+      left: -3px;
+      top: -3px;
+    }
+  }
+</style>
