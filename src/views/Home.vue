b741667844a358df72a066a7b2d76f3fcc8a8af4
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-03 05:45:15 -0800

    Starter VSButton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
+    <VSButton />
   </div>
 </template>
 
 <script>
+import VSButton from '@/components/VSButton';
 
 export default {
   name: 'home',
+  components: {
+    VSButton
+  }
 }
 </script>
