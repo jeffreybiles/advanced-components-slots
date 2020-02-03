c4914e235686c8f7fc2f0310fce1ed76e39a411c
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-03 07:22:07 -0800

    Round to half star

--- src/components/VSRating.vue
+++ src/components/VSRating.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     <span v-for="(item, index) in new Array(5)" :key="index">
-      <slot :isFilled="index <= rating - 1" :isHalf="rating - index == 0.5">
-        <font-awesome-icon icon="star" v-if="index <= rating - 1" />
-        <font-awesome-icon icon="star-half-alt" v-else-if="rating - index == 0.5" />
+      <!-- <slot :isFilled="index <= roundedRating - 1" :isHalf="roundedRating - index == 0.5">
+        <font-awesome-icon icon="star" v-if="index <= roundedRating - 1" />
+        <font-awesome-icon icon="star-half-alt" v-else-if="roundedRating - index == 0.5" />
         <font-awesome-icon :icon="['far', 'star']" v-else />
-      </slot>
-      <!-- <span v-if="index <= rating - 1">
+      </slot> -->
+      <span v-if="index <= roundedRating - 1">
         <slot><font-awesome-icon icon="star" /></slot>
       </span>
-      <span v-else-if="rating - index == 0.5">
+      <span v-else-if="roundedRating - index == 0.5">
         <slot name="half-filled"><font-awesome-icon icon="star-half-alt" /></slot>
       </span>
       <span v-else>
         <slot name="unfilled"><font-awesome-icon :icon="['far', 'star']" /></slot>
-      </span> -->
+      </span>
     </span>
   </div>
 </template>
 
 <script>
   export default {
+    computed: {
+      roundedRating(){
+        return Math.round(this.rating * 2)/2
+      }
+    },
     props: {
       rating: {
         type: Number,
