5a5bad48eb83bc0921e5a66f734a17b16ca9b49d
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-04 01:58:32 -0800

    Part II route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Part I</router-link> |
+      <router-link to="/part2">Part II</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Part2 from '../views/Part2.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/part2',
+    name: 'part2',
+    component: Part2
   }
 ]
 
