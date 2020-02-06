d3e18d447b2d43a82f03af0eafeba54f377bacf9
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-06 15:52:56 -0800

    The method method

--- src/components/VSRatingProps.vue
+++ src/components/VSRatingProps.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <span v-for="(item, index) in new Array(maxRating)" :key="index">
-      <slot :isFilled="index <= roundedRating - 1" :halfFilled="roundedRating - index == 0.5">
-        <font-awesome-icon icon="star" v-if="index <= roundedRating - 1" />
-        <font-awesome-icon icon="star-half-alt" v-else-if="roundedRating - index == 0.5" />
+      <slot :isFilled="isFilled(roundedRating, index)" :halfFilled="halfFilled(roundedRating, index)">
+        <font-awesome-icon icon="star" v-if="isFilled(roundedRating, index)" />
+        <font-awesome-icon icon="star-half-alt" v-else-if="halfFilled(roundedRating, index)" />
         <font-awesome-icon :icon="['far', 'star']" v-else />
       </slot>
     </span>
@@ -15,6 +15,14 @@
     computed: {
       roundedRating(){
         return Math.round(2 * this.rating) / 2
+      }
+    },
+    methods: {
+      isFilled(rating, index) {
+        return index <= rating - 1
+      },
+      halfFilled(rating, index) {
+        return rating - index == 0.5
       }
     },
     props: {
