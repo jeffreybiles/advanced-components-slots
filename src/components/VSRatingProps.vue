0b988344e256cdcb8f9d6605a09ea4fdcf404f03
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-06 15:52:21 -0800

    Exercise 5 - Rounding and Max Rating

--- src/components/VSRatingProps.vue
+++ src/components/VSRatingProps.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <span v-for="(item, index) in new Array(5)" :key="index">
-      <slot :isFilled="index <= rating - 1" :halfFilled="rating - index == 0.5">
-        <font-awesome-icon icon="star" v-if="index <= rating - 1" />
-        <font-awesome-icon icon="star-half-alt" v-else-if="rating - index == 0.5" />
+    <span v-for="(item, index) in new Array(maxRating)" :key="index">
+      <slot :isFilled="index <= roundedRating - 1" :halfFilled="roundedRating - index == 0.5">
+        <font-awesome-icon icon="star" v-if="index <= roundedRating - 1" />
+        <font-awesome-icon icon="star-half-alt" v-else-if="roundedRating - index == 0.5" />
         <font-awesome-icon :icon="['far', 'star']" v-else />
       </slot>
     </span>
@@ -12,10 +12,19 @@
 
 <script>
   export default {
+    computed: {
+      roundedRating(){
+        return Math.round(2 * this.rating) / 2
+      }
+    },
     props: {
       rating: {
         type: Number,
         required: true
+      },
+      maxRating: {
+        type: Number,
+        default: 5
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <VSRatingProps :rating="3.5" />
+    <VSRatingProps :rating="3.8" />
 
-    <VSRatingProps :rating="3.5" v-slot="{isFilled, halfFilled}">
+    <VSRatingProps :rating="8.3" :maxRating="10" v-slot="{isFilled, halfFilled}">
       <font-awesome-icon icon="circle" v-if="isFilled" />
       <font-awesome-icon icon="adjust" v-else-if="halfFilled" />
       <font-awesome-icon :icon="['far', 'circle']" v-else />
