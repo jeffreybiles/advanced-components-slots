d3ad3819c60d56b3fddec0325707a6f6445ceda9
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-06 16:21:50 -0800

    Let Component

--- src/components/VSRatingProps.vue
+++ src/components/VSRatingProps.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
     <span v-for="(item, index) in new Array(maxRating)" :key="index">
-      <slot :isFilled="isFilled(roundedRating, index)" :halfFilled="halfFilled(roundedRating, index)">
-        <font-awesome-icon icon="star" v-if="isFilled(roundedRating, index)" />
-        <font-awesome-icon icon="star-half-alt" v-else-if="halfFilled(roundedRating, index)" />
-        <font-awesome-icon :icon="['far', 'star']" v-else />
-      </slot>
+      <Let :val="index <= roundedRating - 1" v-slot="{val: isFilled}">
+        <slot :isFilled="isFilled" :halfFilled="halfFilled(roundedRating, index)">
+          <font-awesome-icon icon="star" v-if="isFilled" />
+          <font-awesome-icon icon="star-half-alt" v-else-if="halfFilled(roundedRating, index)" />
+          <font-awesome-icon :icon="['far', 'star']" v-else />
+        </slot>
+      </Let>
     </span>
   </div>
 </template>
 
 <script>
+  import Let from '@/components/Let.vue';
   export default {
+    components: {
+      Let
+    },
     computed: {
       roundedRating(){
         return Math.round(2 * this.rating) / 2
       }
     },
     methods: {
-      isFilled(rating, index) {
-        return index <= rating - 1
-      },
       halfFilled(rating, index) {
         return rating - index == 0.5
       }
