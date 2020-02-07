1cbb8a8cdad4ef44b271e7dc1a29b013f61c5b9b
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-06 16:30:02 -0800

    Exercise 6-B

--- src/components/VSRatingProps.vue
+++ src/components/VSRatingProps.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     <span v-for="(item, index) in new Array(maxRating)" :key="index">
-      <Let :val="index <= roundedRating - 1" v-slot="{val: isFilled}">
-        <Let :val="roundedRating - index == 0.5" v-slot="{val: isHalf}">
-          <slot :isFilled="isFilled" :halfFilled="isHalf">
-            <font-awesome-icon icon="star" v-if="isFilled" />
-            <font-awesome-icon icon="star-half-alt" v-else-if="isHalf" />
-            <font-awesome-icon :icon="['far', 'star']" v-else />
-          </slot>
-        </Let>
+      <Let :val="{isFilled: index <= roundedRating - 1,
+                  isHalf: roundedRating - index == 0.5}" v-slot="{val: {isFilled, isHalf}}">
+        <slot :isFilled="isFilled" :halfFilled="isHalf">
+          <font-awesome-icon icon="star" v-if="isFilled" />
+          <font-awesome-icon icon="star-half-alt" v-else-if="isHalf" />
+          <font-awesome-icon :icon="['far', 'star']" v-else />
+        </slot>
       </Let>
     </span>
   </div>
