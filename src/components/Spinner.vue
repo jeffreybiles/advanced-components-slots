27efa3d8fef25221c1fa3afcb241b2f12db0afce
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-19 10:03:16 -0800

    Spinner Component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="loading-spinner">
+      Loading...
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+
+.loading-spinner,
+.loading-spinner:before,
+.loading-spinner:after {
+  background: #000;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loading-spinner {
+  color: #000;
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loading-spinner:before,
+.loading-spinner:after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loading-spinner:before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loading-spinner:after {
+  left: 1.5em;
+}
+
+</style>
