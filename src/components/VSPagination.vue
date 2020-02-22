a5ffcd11a65aa66ce7abe2ab969ea56d5e48019c
Jeffrey Biles <bilesjeffrey@gmail.comgit config -l>
2020-02-22 12:19:56 -0800

    New algo for new perPage selection

--- src/components/VSPagination.vue
+++ src/components/VSPagination.vue
@@ -84,9 +84,15 @@
       },
       changePerPage(resultsPerPage) {
         if(resultsPerPage != this.perPage) {
+          // algorithm so the new results show some of the same items (and don't 'overshoot' and show empty page)
+          if(this.pageNumber > 1) {
+            this.pageNumber = Math.floor(this.perPage / resultsPerPage * this.pageNumber);
+          }
+
           this.perPage = resultsPerPage;
           this.$router.push({path: this.$route.path, query: {
             ...this.$route.query,
+            pageNumber: this.pageNumber,
             perPage: resultsPerPage
           }})
         }
@@ -113,7 +119,7 @@
     border-radius: 3px;
     font-size: 1em;
     cursor: pointer;
-    
+
     &.active {
       background-color: #ccc;
     }
